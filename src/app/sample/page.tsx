e5b0a6f079cf991a6837ec4da92284a4919c9199
Nielleeeee <jandnllplz@gmail.com>
2024-11-17 04:42:58 +0800

    chore: try adding another page

---
+++ src/app/sample/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Sample() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <section className="container mx-auto p-8 text-center">
+        <h1 className="text-4xl font-bold">Sample page</h1>
+        <p className="text-xl">This is a sample page.</p>
+      </section>
+    </main>
+  );
+}
